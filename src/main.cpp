352beaef913126bf6e131a7a862155c1c202af54
mo2menelzeiny <mo2menelzeiny@gmail.com>
2018-08-08 01:11:19 +0200

    Implemented and tested arbitrage data recorder Fixed missing swissquote tags Fixed swissquote filter expression and tag selection

--- src/main.cpp
+++ src/main.cpp
@@ -160,10 +160,19 @@ int main() {
 		broker_market_data_disruptor->start();
 		arbitrage_data_disruptor->start();
 
+
+		auto arbitrage_recorder_poller = arbitrage_data_disruptor->ringBuffer()->newPoller();
+		auto arbitrage_recorder_handler = [&](ArbitrageDataEvent &data, std::int64_t sequence,
+		                                      bool endOfBatch) -> bool {
+			recorder->recordArbitrage(data);
+			return true;
+		};
+
 		recorder->recordSystem("Main: all OK", SYSTEM_RECORD_TYPE_SUCCESS);
 
 		while (true) {
-			std::this_thread::yield();
+			arbitrage_recorder_poller->poll(arbitrage_recorder_handler);
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		}
 
 	} catch (const std::exception &e) {
