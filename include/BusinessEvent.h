1977899b52803a7d9ee21b40d410e56046263181
mo2menelzeiny <mo2menelzeiny@gmail.com>
2018-09-18 03:44:01 +0200

    Implemented business recovery data of open side and orders count Fixed open side enum Refactored system record type Increased buffer size

--- include/BusinessEvent.h
+++ include/BusinessEvent.h
@@ -2,12 +2,16 @@
 #ifndef ARBITO_BUSINESSEVENT_H
 #define ARBITO_BUSINESSEVENT_H
 
+#include "OpenSide.h"
+
 struct BusinessEvent {
 	char side;
 	int clOrdId;
 	double trigger_px;
 	double remote_px;
 	long timestamp_us;
+	OpenSide open_side;
+	int orders_count;
 };
 
 #endif //ARBITO_BUSINESSEVENT_H
