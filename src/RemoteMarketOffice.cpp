6d8bfb6ec5f94aa75b151ebc0111ac5c9bc0d469
mo2menelzeiny <mo2menelzeiny@gmail.com>
2018-09-21 04:54:06 +0200

    refactor(control): remove control same source condition

--- src/RemoteMarketOffice.cpp
+++ src/RemoteMarketOffice.cpp
@@ -18,10 +18,6 @@ void RemoteMarketOffice::poll() {
 	auto control_poller = m_control_buffer->newPoller();
 	m_control_buffer->addGatingSequences({control_poller->sequence()});
 	auto control_handler = [&](ControlEvent &data, std::int64_t sequence, bool endOfBatch) -> bool {
-		if (data.source == CES_REMOTE_M_OFFICE) {
-			return false;
-		}
-
 		switch (data.type) {
 			case CET_PAUSE:
 				pause = true;
