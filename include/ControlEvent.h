3be370f8dfe513521f9eeeaefcce183bcec92c14
mo2menelzeiny <mo2menelzeiny@gmail.com>
2018-09-21 07:38:55 +0200

    feat(exclusive): send control events to test back pressure

--- include/ControlEvent.h
+++ include/ControlEvent.h
@@ -3,21 +3,24 @@
 #define ARBITO_CONTROLEVENT_H
 enum ControlEventSource {
 	CES_TRADE_OFFICE = 0,
-	CES_BUSINESS_OFFICE = 1,
-	CES_MARKET_OFFICE = 2,
+	CES_MARKET_OFFICE = 1,
+	CES_BUSINESS_OFFICE = 2,
 	CES_REMOTE_M_OFFICE = 3,
 	CES_RECORDS_OFFICE = 4,
-	CES_MAIN = 5
+	CES_EXCLUSIVE_M_OFFICE = 5,
+	CES_MAIN = 6
 };
 
 enum ControlEventType {
 	CET_PAUSE = 0,
-	CET_RESUME = 1
+	CET_RESUME = 1,
+	CET_BP = 2
 };
 struct ControlEvent {
 	ControlEventSource source;
 	ControlEventType type;
 	long timestamp_us;
+	int back_pressure;
 };
 
 #endif //ARBITO_CONTROLEVENT_H
