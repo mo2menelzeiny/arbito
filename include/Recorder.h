fd019c0cb099fddffedde82ea95a506757ecc857
mo2menelzeiny <mo2menelzeiny@gmail.com>
2018-10-02 06:19:21 +0200

    refactor(recorder): publish records from inside office polling thread

--- include/Recorder.h
+++ include/Recorder.h
@@ -37,9 +37,14 @@ public:
 
 private:
 
-	void pollBuffers();
+	void poll();
 
-	void pollRecords();
+public:
+	std::shared_ptr<Disruptor::RingBuffer<ControlEvent>> m_control_records_buffer;
+	std::shared_ptr<Disruptor::RingBuffer<MarketDataEvent>> m_local_records_buffer;
+	std::shared_ptr<Disruptor::RingBuffer<RemoteMarketDataEvent>> m_remote_records_buffer;
+	std::shared_ptr<Disruptor::RingBuffer<BusinessEvent>> m_business_records_buffer;
+	std::shared_ptr<Disruptor::RingBuffer<TradeEvent>> m_trade_records_buffer;
 
 private:
 	const std::shared_ptr<Disruptor::RingBuffer<ControlEvent>> m_control_buffer;
@@ -47,17 +52,11 @@ private:
 	const std::shared_ptr<Disruptor::RingBuffer<RemoteMarketDataEvent>> m_remote_md_buffer;
 	const std::shared_ptr<Disruptor::RingBuffer<BusinessEvent>> m_business_buffer;
 	const std::shared_ptr<Disruptor::RingBuffer<TradeEvent>> m_trade_buffer;
-	std::shared_ptr<Disruptor::RingBuffer<ControlEvent>> m_control_records_buffer;
-	std::shared_ptr<Disruptor::RingBuffer<MarketDataEvent>> m_local_records_buffer;
-	std::shared_ptr<Disruptor::RingBuffer<RemoteMarketDataEvent>> m_remote_records_buffer;
-	std::shared_ptr<Disruptor::RingBuffer<BusinessEvent>> m_business_records_buffer;
-	std::shared_ptr<Disruptor::RingBuffer<TradeEvent>> m_trade_records_buffer;
 	mongoc_uri_t *m_uri;
 	mongoc_client_pool_t *m_pool;
 	const char *m_broker_name;
 	const char *m_db_name;
-	std::thread m_buffers_poller;
-	std::thread m_records_poller;
+	std::thread m_poller;
 };
 
 
