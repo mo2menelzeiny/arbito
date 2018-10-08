561cbf4c8b6b17aa0d21fb2e5d23026513a5a3db
mo2menelzeiny <mo2menelzeiny@gmail.com>
2018-10-08 06:30:27 +0200

    fix(socket): close socket on disconnect

--- src/lmax/TradeOffice.cpp
+++ src/lmax/TradeOffice.cpp
@@ -267,12 +267,13 @@ namespace LMAX {
 		m_recorder->recordSystemMessage("TradeOffice: Connection FAILED", SYSTEM_RECORD_TYPE_ERROR);
 		fprintf(stderr, "TradeOffice: Connection FAILED\n");
 
-		std::this_thread::sleep_for(std::chrono::seconds(RECONNECT_DELAY_SEC));
-
-		SSL_free(m_cfg.ssl);
+		close(m_session->sockfd);
+		SSL_free(m_session->ssl);
 		ERR_free_strings();
 		EVP_cleanup();
 		lmax_fix_session_free(m_session);
+
+		std::this_thread::sleep_for(std::chrono::seconds(RECONNECT_DELAY_SEC));
 
 		connectToBroker();
 
