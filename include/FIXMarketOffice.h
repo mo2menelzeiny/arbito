522a11d07f6503c46f9c4ba9afdeb09423f1ee09
mo2menelzeiny <mo2menelzeiny@gmail.com>
2018-11-08 00:56:37 +0200

    feat(FIXSession): market office can send prices to business office

--- include/FIXMarketOffice.h
+++ include/FIXMarketOffice.h
@@ -2,12 +2,28 @@
 #ifndef ARBITO_FIXMARKETOFFICE_H
 #define ARBITO_FIXMARKETOFFICE_H
 
+// Aeron client
+#include <Context.h>
+#include <Aeron.h>
+#include <concurrent/NoOpIdleStrategy.h>
+#include <FragmentAssembler.h>
+
+// SBE
+#include "sbe/sbe.h"
+#include "sbe/MarketData.h"
+
+// Domain
+#include "Config.h"
 #include "FIXSession.h"
 
 class FIXMarketOffice {
 public:
 	FIXMarketOffice(
 			const char *broker,
+			double spread,
+			double quantity,
+			const char *BOHost,
+			int BOPort,
 			const char *host,
 			int port,
 			const char *username,
@@ -20,10 +36,14 @@ public:
 	void start();
 
 private:
-	void poll();
+	void work();
 
 private:
 	const char *m_broker;
+	double m_spread;
+	double m_quantity;
+	const char *m_BOHost;
+	int m_BOPort;
 	struct fix_field *m_MDRFields;
 	struct fix_message m_MDRFixMessage{};
 	FIXSession *m_fixSession;
