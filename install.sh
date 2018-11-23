17aad83643912de17b55af4bdd083d78721910e9
mo2menelzeiny <mo2menelzeiny@gmail.com>
2018-11-23 15:07:31 +0200

    refactor: removed disruptor and unused classes

--- install.sh
+++ install.sh
@@ -3,19 +3,6 @@
 mkdir externals
 cd externals
 
-echo "Installing Disruptor.."
-wget https://github.com/Abc-Arbitrage/Disruptor-cpp/archive/master.zip
-unzip master.zip
-rm master.zip
-cd Disruptor-cpp-master
-mkdir build
-cd build
-cmake .. -DDISRUPTOR_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
-make -j 4 install PREFIX=/usr/local
-
-#return to externals/
-cd ../..
-
 echo "Installing Aeron.."
 wget https://github.com/real-logic/aeron/archive/1.11.3.tar.gz
 tar zvxf 1.11.3.tar.gz
