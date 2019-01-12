42bd4f1a2d2e28403dc8e76ccdfb092640fc5ef3
mo2menelzeiny <mo2menelzeiny@gmail.com>
2019-01-12 20:09:12 +0200

    refactor(business-office): implement invokable duty cycle

--- include/BusinessOffice.h
+++ include/BusinessOffice.h
@@ -2,19 +2,18 @@
 #ifndef ARBITO_BUSINESSOFFICE_H
 #define ARBITO_BUSINESSOFFICE_H
 
-// SPDLOG
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/daily_file_sink.h"
-
 // Disruptor
-#include "Disruptor/Disruptor.h"
+#include <Disruptor/Disruptor.h>
+
+// SPDLOG
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/daily_file_sink.h>
+
 
 // Domain
 #include "BusinessEvent.h"
 #include "MarketDataEvent.h"
 #include "TriggerDifference.h"
-
-// MongoC
 #include "MongoDBDriver.h"
 
 using namespace std;
@@ -25,7 +24,6 @@ public:
 	BusinessOffice(
 			std::shared_ptr<Disruptor::RingBuffer<MarketDataEvent>> &inRingBuffer,
 			std::shared_ptr<Disruptor::RingBuffer<BusinessEvent>> &outRingBuffer,
-			int cpuset,
 			int windowMs,
 			int orderDelaySec,
 			int maxOrders,
@@ -35,25 +33,171 @@ public:
 			const char *dbName
 	);
 
-	void start();
+	inline void doWork() {
+		m_marketDataEventPoller->poll(m_marketDataEventHandler);
 
-	void stop();
+		if (m_isOrderDelayed && ((time(nullptr) - m_lastOrderTime) < m_orderDelaySec)) {
+			return;
+		}
 
-private:
-	void work();
+		m_isOrderDelayed = false;
+
+		bool canOpen = m_ordersCount < m_maxOrders;
+
+		double diffA = m_marketDataA.bid - m_marketDataB.offer;
+		double diffB = m_marketDataB.bid - m_marketDataA.offer;
+
+		switch (m_currentDiff) {
+			case DIFF_A:
+				if (canOpen && diffA >= m_diffOpen) {
+					m_currentOrder = DIFF_A;
+					++m_ordersCount;
+					break;
+				}
+
+				if (diffB >= m_diffClose) {
+					m_currentOrder = DIFF_B;
+					--m_ordersCount;
+					break;
+				}
+
+			case DIFF_B:
+				if (canOpen && diffB >= m_diffOpen) {
+					m_currentOrder = DIFF_B;
+					++m_ordersCount;
+					break;
+				}
+
+				if (diffA >= m_diffClose) {
+					m_currentOrder = DIFF_A;
+					--m_ordersCount;
+					break;
+				}
+
+			case DIFF_NONE:
+				if (diffA >= m_diffOpen) {
+					m_currentDiff = DIFF_A;
+					m_currentOrder = DIFF_A;
+					++m_ordersCount;
+					break;
+				}
+
+				if (diffB >= m_diffOpen) {
+					m_currentDiff = DIFF_B;
+					m_currentOrder = DIFF_B;
+					++m_ordersCount;
+					break;
+				}
+
+		}
+
+		if (m_ordersCount == 0) {
+			m_currentDiff = DIFF_NONE;
+		}
+
+		if (m_currentOrder == DIFF_NONE) {
+			return;
+		}
+
+		const char *orderType = m_currentOrder == m_currentDiff ? "OPEN" : "CLOSE";
+
+		auto randomId = m_idGenerator();
+		sprintf(m_randomIdStrBuff, "%lu", randomId);
+
+		auto nextSequence = m_outRingBuffer->next();
+		(*m_outRingBuffer)[nextSequence].id = randomId;
+
+		switch (m_currentOrder) {
+			case DIFF_A:
+
+				(*m_outRingBuffer)[nextSequence].sell = m_marketDataA.broker;
+				(*m_outRingBuffer)[nextSequence].buy = m_marketDataB.broker;
+				m_outRingBuffer->publish(nextSequence);
+
+				m_systemLogger->info(
+						"{} bid: {} sequence: {} offer: {} sequence: {}",
+						orderType,
+						m_marketDataA.bid,
+						m_marketDataA.sequence,
+						m_marketDataB.offer,
+						m_marketDataB.sequence
+				);
+
+				std::thread([mongoDriver = &m_mongoDriver,
+						            id = m_randomIdStrBuff,
+						            a = m_marketDataA,
+						            b = m_marketDataB,
+						            orderType] {
+					mongoDriver->record(id, a.bid, b.offer, orderType);
+				}).detach();
+
+				break;
+
+			case DIFF_B:
+				(*m_outRingBuffer)[nextSequence].sell = m_marketDataB.broker;
+				(*m_outRingBuffer)[nextSequence].buy = m_marketDataA.broker;
+				m_outRingBuffer->publish(nextSequence);
+
+				m_systemLogger->info(
+						"{} bid: {} sequence: {} offer: {} sequence: {}",
+						orderType,
+						m_marketDataB.bid,
+						m_marketDataB.sequence,
+						m_marketDataA.offer,
+						m_marketDataA.sequence
+				);
+
+				std::thread([mongoDriver = &m_mongoDriver,
+						            id = m_randomIdStrBuff,
+						            a = m_marketDataA,
+						            b = m_marketDataB,
+						            orderType] {
+					mongoDriver->record(id, b.bid, a.offer, orderType);
+				}).detach();
+
+				break;
+
+			case DIFF_NONE:
+				break;
+		}
+
+		m_marketDataA.bid = -99;
+		m_marketDataA.offer = 99;
+
+		m_marketDataB.bid = -99;
+		m_marketDataB.offer = 99;
+
+		m_currentOrder = DIFF_NONE;
+		m_lastOrderTime = time(nullptr);
+		m_isOrderDelayed = true;
+
+		m_consoleLogger->info("{}", orderType);
+	}
+
+	void cleanup();
 
 private:
 	std::shared_ptr<Disruptor::RingBuffer<MarketDataEvent>> m_inRingBuffer;
 	std::shared_ptr<Disruptor::RingBuffer<BusinessEvent>> m_outRingBuffer;
-	int m_cpuset;
 	int m_windowMs;
 	int m_orderDelaySec;
 	int m_maxOrders;
 	double m_diffOpen;
 	double m_diffClose;
-	std::thread m_worker;
-	std::atomic_bool m_running;
 	MongoDBDriver m_mongoDriver;
+	std::shared_ptr<spdlog::logger> m_consoleLogger;
+	std::shared_ptr<spdlog::logger> m_systemLogger;
+	std::shared_ptr<Disruptor::EventPoller<MarketDataEvent>> m_marketDataEventPoller;
+	std::function<bool(MarketDataEvent &, long, bool)> m_marketDataEventHandler;
+	MarketDataEvent m_marketDataA;
+	MarketDataEvent m_marketDataB;
+	int m_ordersCount;
+	TriggerDifference m_currentDiff;
+	TriggerDifference m_currentOrder;
+	std::mt19937_64 m_idGenerator;
+	char m_randomIdStrBuff[64];
+	bool m_isOrderDelayed;
+	time_t m_lastOrderTime;
 };
 
 
--- src/BusinessOffice.cpp
+++ src/BusinessOffice.cpp
@@ -1,10 +1,9 @@
 
-#include "BusinessOffice.h"
+#include <BusinessOffice.h>
 
 BusinessOffice::BusinessOffice(
 		std::shared_ptr<Disruptor::RingBuffer<MarketDataEvent>> &inRingBuffer,
 		std::shared_ptr<Disruptor::RingBuffer<BusinessEvent>> &outRingBuffer,
-		int cpuset,
 		int windowMs,
 		int orderDelaySec,
 		int maxOrders,
@@ -13,7 +12,7 @@ BusinessOffice::BusinessOffice(
 		const char *dbUri,
 		const char *dbName
 ) : m_inRingBuffer(inRingBuffer),
-	m_cpuset(cpuset),
+    m_outRingBuffer(outRingBuffer),
     m_windowMs(windowMs),
     m_orderDelaySec(orderDelaySec),
     m_maxOrders(maxOrders),
@@ -24,213 +23,55 @@ BusinessOffice::BusinessOffice(
 		    dbName,
 		    "coll_triggers"
     ) {
-}
+	m_idGenerator = std::mt19937_64(std::random_device{}());
 
-void BusinessOffice::start() {
-	m_running = true;
-	m_worker = std::thread(&BusinessOffice::work, this);
-}
+	m_consoleLogger = spdlog::get("console");
+	m_systemLogger = spdlog::get("system");
 
-void BusinessOffice::stop() {
-	m_running = false;
-	m_worker.join();
-}
+	m_ordersCount = stoi(getenv("ORDERS_COUNT"));
 
-void BusinessOffice::work() {
-	cpu_set_t cpuset;
-	CPU_ZERO(&cpuset);
-	CPU_SET(m_cpuset, &cpuset);
-	pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
-	pthread_setname_np(pthread_self(), "business-office");
+	m_currentDiff = DIFF_NONE;
+	m_currentOrder = DIFF_NONE;
 
-	MarketDataEvent marketDataEmpty = (MarketDataEvent) {
-			.bid = -99,
-			.offer = 99,
-			.sequence = 0,
-			.broker = NONE
-	};
+	if (!strcmp(getenv("CURRENT_DIFF"), "DIFF_A")) {
+		m_currentDiff = DIFF_A;
+	}
 
-	MarketDataEvent marketDataA = marketDataEmpty, marketDataB = marketDataEmpty;
+	if (!strcmp(getenv("CURRENT_DIFF"), "DIFF_B")) {
+		m_currentDiff = DIFF_B;
+	}
 
-	int ordersCount = stoi(getenv("ORDERS_COUNT"));
+	m_isOrderDelayed = false;
+	m_lastOrderTime = time(nullptr);
 
-	TriggerDifference currentDiff = DIFF_NONE;
+	m_marketDataEventPoller = m_inRingBuffer->newPoller();
+	m_inRingBuffer->addGatingSequences({m_marketDataEventPoller->sequence()});
 
-	if (!strcmp(getenv("CURRENT_DIFF"), "DIFF_A")) currentDiff = DIFF_A;
-
-	if (!strcmp(getenv("CURRENT_DIFF"), "DIFF_B")) currentDiff = DIFF_B;
-
-	TriggerDifference currentOrder = DIFF_NONE;
-
-	/*bool isExpiredA = true, isExpiredB = true;
-	time_point<system_clock> timestampA, timestampB, timestampNow;
-	timestampA = timestampB = timestampNow = system_clock::now();*/
-
-	bool isOrderDelayed = false;
-	time_t orderDelay = m_orderDelaySec;
-	time_t orderDelayStart = time(nullptr);
-
-	auto consoleLogger = spdlog::get("console");
-	auto systemLogger = spdlog::get("system");
-
-	std::mt19937_64 randomGenerator(std::random_device{}());
-
-	auto handleTriggers = [&] {
-		if (isOrderDelayed && ((time(nullptr) - orderDelayStart) < orderDelay)) return;
-
-		isOrderDelayed = false;
-
-		bool canOpen = ordersCount < m_maxOrders;
-
-		double diffA = marketDataA.bid - marketDataB.offer;
-		double diffB = marketDataB.bid - marketDataA.offer;
-
-		switch (currentDiff) {
-			case DIFF_A:
-				if (canOpen && diffA >= m_diffOpen) {
-					currentOrder = DIFF_A;
-					++ordersCount;
-					break;
-				}
-
-				if (diffB >= m_diffClose) {
-					currentOrder = DIFF_B;
-					--ordersCount;
-					break;
-				}
-
-			case DIFF_B:
-				if (canOpen && diffB >= m_diffOpen) {
-					currentOrder = DIFF_B;
-					++ordersCount;
-					break;
-				}
-
-				if (diffA >= m_diffClose) {
-					currentOrder = DIFF_A;
-					--ordersCount;
-					break;
-				}
-
-			case DIFF_NONE:
-				if (diffA >= m_diffOpen) {
-					currentDiff = DIFF_A;
-					currentOrder = DIFF_A;
-					++ordersCount;
-					break;
-				}
-
-				if (diffB >= m_diffOpen) {
-					currentDiff = DIFF_B;
-					currentOrder = DIFF_B;
-					++ordersCount;
-					break;
-				}
-
-		}
-
-		if (ordersCount == 0) {
-			currentDiff = DIFF_NONE;
-		}
-
-		if (currentOrder == DIFF_NONE) return;
-
-		const char *orderType = currentOrder == currentDiff ? "OPEN" : "CLOSE";
-
-		auto randomId = randomGenerator();
-
-		char randomIdStr[64];
-
-		sprintf(randomIdStr, "%lu", randomId);
-
-		auto nextSequence = m_outRingBuffer->next();
-
-		(*m_outRingBuffer)[nextSequence].id = randomId;
-
-		switch (currentOrder) {
-			case DIFF_A:
-
-				(*m_outRingBuffer)[nextSequence].sell = marketDataA.broker;
-				(*m_outRingBuffer)[nextSequence].buy = marketDataB.broker;
-				m_outRingBuffer->publish(nextSequence);
-
-				systemLogger->info(
-						"{} bid: {} sequence: {} offer: {} sequence: {}",
-						orderType,
-						marketDataA.bid,
-						marketDataA.sequence,
-						marketDataB.offer,
-						marketDataB.sequence
-				);
-
-				std::thread([=, mongoDriver = &m_mongoDriver] {
-					mongoDriver->record(randomIdStr, marketDataA.bid, marketDataB.offer, orderType);
-				}).detach();
-
-				break;
-
-			case DIFF_B:
-				(*m_outRingBuffer)[nextSequence].sell = marketDataB.broker;
-				(*m_outRingBuffer)[nextSequence].buy = marketDataA.broker;
-				m_outRingBuffer->publish(nextSequence);
-
-				systemLogger->info(
-						"{} bid: {} sequence: {} offer: {} sequence: {}",
-						orderType,
-						marketDataB.bid,
-						marketDataB.sequence,
-						marketDataA.offer,
-						marketDataA.sequence
-				);
-
-				std::thread([=, mongoDriver = &m_mongoDriver] {
-					mongoDriver->record(randomIdStr, marketDataB.bid, marketDataA.offer, orderType);
-				}).detach();
-
-				break;
-
-			case DIFF_NONE:
-				break;
-		}
-
-		marketDataA.bid = -99;
-		marketDataA.offer = 99;
-
-		marketDataB.bid = -99;
-		marketDataB.offer = 99;
-
-		currentOrder = DIFF_NONE;
-		orderDelayStart = time(nullptr);
-		isOrderDelayed = true;
-
-		consoleLogger->info("{}", orderType);
-	};
-
-	auto marketDataPoller = m_inRingBuffer->newPoller();
-
-	m_inRingBuffer->addGatingSequences({marketDataPoller->sequence()});
-
-	auto marketDataHandler = [&](MarketDataEvent &event, int64_t seq, bool endOfBatch) -> bool {
-		switch (event.broker) {
+	m_marketDataEventHandler = [&](MarketDataEvent &marketDataEvent, int64_t seq, bool endOfBatch) -> bool {
+		switch (marketDataEvent.broker) {
 			case LMAX:
-				marketDataA = event;
+				m_marketDataA = marketDataEvent;
 				break;
 			case IB:
-				marketDataB = event;
+				m_marketDataB = marketDataEvent;
 				break;
 			case SWISSQUOTE:
 			case NONE:
 				break;
 		}
+
 		return false;
 	};
 
-	consoleLogger->info("Central Office OK");
+	m_marketDataA.bid = -99;
+	m_marketDataA.offer = 99;
 
-	while (m_running) {
-		marketDataPoller->poll(marketDataHandler);
-		handleTriggers();
-	}
+	m_marketDataB.bid = -99;
+	m_marketDataB.offer = 99;
 
-	m_inRingBuffer->removeGatingSequence(marketDataPoller->sequence());
+	m_consoleLogger->info("Business Office OK");
+}
+
+void BusinessOffice::cleanup() {
+	m_inRingBuffer->removeGatingSequence(m_marketDataEventPoller->sequence());
 }
