e12e40520991e844bcc68cb97f31279caf0dfaae
mo2menelzeiny <mo2menelzeiny@gmail.com>
2019-01-15 19:47:52 +0200

    test: trigger only

--- src/main.cpp
+++ src/main.cpp
@@ -108,9 +108,9 @@ int main() {
 
 		businessOffice.start();
 		marketOfficeA.start();
-		tradeOfficeA.start();
+//		tradeOfficeA.start();
 		marketOfficeB.start();
-		tradeOfficeB.start();
+//		tradeOfficeB.start();
 
 		consoleLogger->info("Main OK");
 
