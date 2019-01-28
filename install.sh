cdfd0725f45d8559760b76d7c5af17d6f448bde3
mo2menelzeiny <mo2menelzeiny@gmail.com>
2019-01-28 09:45:57 +0200

    chore: cleanup apt and install script

--- install.sh
+++ install.sh
@@ -10,28 +10,14 @@ rm master.zip
 cd Disruptor-cpp-master
 mkdir build
 cd build
-cmake .. -DDISRUPTOR_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
-make -j 2 install PREFIX=/usr/local
-
-#return to externals/
-cd ../..
-
-echo "Installing Mongo C Driver.."
-wget https://github.com/mongodb/mongo-c-driver/releases/download/1.12.0/mongo-c-driver-1.12.0.tar.gz
-tar xzf mongo-c-driver-1.12.0.tar.gz
-rm mongo-c-driver-1.12.0.tar.gz
-cd mongo-c-driver-1.12.0
-mkdir cmake-build
-cd cmake-build
-cmake .. -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -DCMAKE_BUILD_TYPE=Release
-make -j 2 install PREFIX=/usr/local
+cmake .. -DCMAKE_BUILD_TYPE=Release -DDISRUPTOR_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX:PATH=/usr/local
+cmake --build . --target install -- -j 2
 
 #Leave to root
 cd ../../..
-
 
 echo "Building executables.."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j 2
+cmake --build . --target arbito -- -j 2
